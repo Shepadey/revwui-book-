e510fa3fa26472167eddb6ee2cdc95926d087d2a
Shepadey <dolechagy@gmail.com>
2022-03-06 12:43:31 +0300

    new reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,12 @@
+from reviews.forms import ReviewForm
+
+
+from reviews.forms import ReviewForm
 from django.shortcuts import render
 def reviews(request):
-    return render(request,'reviews.html')
+    form=ReviewForm()
+    name_1=request.GET.get('name')
+    email_1=request.GET.get('email')
+    review_1=request.GET.get('review')
+    return render(request,'reviews.html',{'name_1':name_1,'email_1':email_1,'review_1':review_1,'form':form})
 # Create your views here.
