1c365644e105e719a12c4e51193d0edc8b058046
Shepadey <dolechagy@gmail.com>
2022-03-20 12:36:44 +0300

    new suspects

--- reviews/views.py
+++ reviews/views.py
@@ -3,7 +3,26 @@ from reviews.forms import ReviewForm
 
 from reviews.forms import ReviewForm
 from django.shortcuts import render
+from django.shortcuts import redirect
 def reviews(request):
+    if request.method=="GET":
+        form=ReviewForm()
+        return render(request,"reviews.html",{'form':form})
+    elif request.method=='POST':
+        form=ReviewForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+            name=data.get('name')
+            email=data.get('email')
+            review=data.get('reveiw')
+            rating=data.get('rating')
+            print(data)
+            with open ('data.csv','a') as file:
+                file.write(f'{name}|{email}|{review}|{rating}\n')
+            return redirect('reviews')
+        else:
+            form=ReviewForm()
+            return render(request,'reviews.html',{'form':form})
     form=ReviewForm()
     name_1=request.GET.get('name')
     email_1=request.GET.get('email')
